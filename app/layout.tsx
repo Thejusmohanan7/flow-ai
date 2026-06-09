bd017cc9bb461323a4533701f3e90f98fa11131d
Thejusmohanan7 <135072883+Thejusmohanan7@users.noreply.github.com>
2026-06-09 23:12:52 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider>
       <html
         lang="en"
         suppressHydrationWarning
